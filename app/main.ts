13e08291bf8dae25490985f20f3c9156a7d051e1
rinshadpibnabu <rinshadpibnabu@gmail.com>
2025-04-22 09:09:46 +0530

    [debug]

--- app/main.ts
+++ app/main.ts
@@ -13,7 +13,7 @@ const server = net.createServer((socket) => {
     if (path === "/index.html") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
-      socket.write("HTTP/1.1 400 Not Found\r\n\r\n");
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
   socket.on("close", () => {
