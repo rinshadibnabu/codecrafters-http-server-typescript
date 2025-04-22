620e79e987b8d3907bd6ab237ba23ffaa1e2d0a6
rinshadpibnabu <rinshadpibnabu@gmail.com>
2025-04-22 07:29:34 +0530

    add the responese status line
    learn to implement a respone to a request using  net module socket connetion

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
   socket.on("close", () => {
+    console.log("client got disconnected");
     socket.end();
+  });
+  socket.on("error", (err) => {
+    console.log(`socket error ${err}`);
   });
 });
 
