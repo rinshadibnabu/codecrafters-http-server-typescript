47795cd0613f47552be9ef95b72e20486deffb2f
rinshadibnabu <rinshadpibnabu@gmail.com>
2025-04-26 07:40:57 +0530

    move response builder to separate file

--- app/main.ts
+++ app/main.ts
@@ -1,40 +1,13 @@
-import type { BunFile } from "bun";
+import type { BunFile, Socket } from "bun";
 import * as net from "node:net";
 import path from "node:path";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 import { pasrseReq } from "./requestParser";
-function buildResponse(
-  status: { code: number; text: string },
-  headers: Record<string, string>,
-  body: string | Uint8Array,
-): Uint8Array {
-  const statusLine = `HTTP/1.1 ${status.code} ${status.text}`;
-
-  const headerLines = Object.entries(headers)
-    .map(([key, value]) => `${key}: ${value}`)
-    .join("\r\n");
-  const head = `${statusLine}\r\n${headerLines}\r\n\r\n`;
-
-  const headBytes = new TextEncoder().encode(head);
-
-  if (typeof body === "string") {
-    const bodyBytes = new TextEncoder().encode(body);
-    const full = new Uint8Array(headBytes.length + bodyBytes.length);
-    full.set(headBytes);
-    full.set(bodyBytes, headBytes.length);
-    return full;
-  } else {
-    const full = new Uint8Array(headBytes.length + body.length);
-    full.set(headBytes);
-    full.set(body, headBytes.length);
-    return full;
-  }
-}
-
-const server = net.createServer((socket) => {
-  socket.on("data", async (data) => {
+import { buildResponse } from "./responseBuilder";
+const server = net.createServer((socket): void => {
+  socket.on("data", async (data: Buffer): Promise<void> => {
     const rawData = data.toString();
     const req = pasrseReq(rawData);
 
---
+++ app/responseBuilder.ts
@@ -0,0 +1,27 @@
+export function buildResponse(
+  status: { code: number; text: string },
+  headers: Record<string, string>,
+  body: string | Uint8Array,
+): Uint8Array {
+  const statusLine = `HTTP/1.1 ${status.code} ${status.text}`;
+
+  const headerLines = Object.entries(headers)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join("\r\n");
+  const head = `${statusLine}\r\n${headerLines}\r\n\r\n`;
+
+  const headBytes = new TextEncoder().encode(head);
+
+  if (typeof body === "string") {
+    const bodyBytes = new TextEncoder().encode(body);
+    const full = new Uint8Array(headBytes.length + bodyBytes.length);
+    full.set(headBytes);
+    full.set(bodyBytes, headBytes.length);
+    return full;
+  } else {
+    const full = new Uint8Array(headBytes.length + body.length);
+    full.set(headBytes);
+    full.set(body, headBytes.length);
+    return full;
+  }
+}
